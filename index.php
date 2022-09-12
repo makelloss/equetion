da3c7fe070a04043c192305cf557e9fd5bfa4d53
makelloss <artemvakhno@gmail.com>
2022-09-12 15:33:14 +0300

    https://github.com/makelloss/temp.git

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $b = 0;
-$a = 6.464645;
+$a = -6.464645;
 //echo round ($a);
 if ($a > $b) {
-    echo "Тепло!";
+    echo "<span style = \" color: red;\">Тепло!</span>";
 } elseif ($a == $b) {
     echo "Не мороз и не тепло…";
 } else {
-    echo "Мороз!";
+    echo "<span style = \" color: blue;\">Мороз!</span>";
 }
 ?>
